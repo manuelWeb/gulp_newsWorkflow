f4756b1fefce768193c9bd93f4b2208f8ef8a10c
Manu Lannoy <devries.webdesigner@gmail.com>
2015-10-31 17:26:00 +0100

    simple slim & sass > ok;-b simple1

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ slim        = require("gulp-slim"),
 sass        = require('gulp-sass'),
 premailer   = require('gulp-premailer');
 
-
+// var dist = ( 'dist/' );
 /*=================================
 =            task init            =
 =================================*/
@@ -18,61 +18,52 @@ premailer   = require('gulp-premailer');
 gulp.task('browserSync',function () {
   browserSync({
     server: {
-      baseDir: 'render'
+      baseDir: 'render/BV/html/'
     }
   })
 })
 
 // sass task
 gulp.task('sass', function() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src('src/**/scss/*.scss')
   .pipe(sass())
-  .pipe(gulp.dest('render/css'))
+  .pipe(gulp.dest('render'))
   .pipe(browserSync.reload({
     stream: true
   }))
 })
 // slim task
 gulp.task('slim', function () {
-  return gulp.src('src/**/*.slim')
+  return gulp.src('src/**/slim/*.slim')
   .pipe(slim({
-    pretty: true // ,require: 'slim/include'
+    pretty: true
   }))
-  .pipe(gulp.dest('render/html'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
+  .pipe(gulp.dest('render'))
+  // .pipe(browserSync.reload({
+  //   stream: true
+  // }));
 });
 // premailer task
-gulp.task('premailer', function () {
-  gulp.src('render/html/index.html')
-  .pipe(premailer())
-  .pipe(gulp.dest('render/'));
-});
-
-// BV
-// premailerBV task
-gulp.task('premailerBV', function () {
-  gulp.src('render/html/BV/index.html')
-  .pipe(premailer())
-  .pipe(gulp.dest('render/html/BV/html/'))
-});
-// sass2 task
-gulp.task('sass2', function() {
-  return gulp.src('src/BV/scss/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('render/html/BV/css'))
-})
+// gulp.task('premailer', function () {
+//   gulp.src('src/**/html/index.html')
+//   .pipe(premailer())
+//   .pipe(gulp.dest('src/'));
+// });
 
 // lancement > fonction watch
-gulp.task(
-  'watch',
-  ['browserSync', 'slim', 'sass', 'sass2', 'premailerBV', 'premailer'],
-  function () {
-  gulp.watch('src/scss/*.scss',['sass']);
-  gulp.watch('src/**/*.slim',['slim']);
-  gulp.watch('render/html/index.html',['premailer']);
-  // BV
-  gulp.watch('src/BV/scss/*.scss',['sass2']);
-  gulp.watch('render/html/BV/index.html',['premailerBV']);
-})
+gulp.task( 'build',['slim','sass'], function() {
+  // gulp.watch( 'browserSync','slim', 'sass', 'imgs' );
+  gulp.watch('src/**/slim/*.slim',['slim']);
+  gulp.watch('src/**/scss/*.scss',['sass']);
+});
+
+
+
+// gulp.task('watch', ['browserSync', 'slim', 'sass'], function () {
+//   gulp.watch('src/scss/*.scss',['sass']);
+//   gulp.watch('src/**/*.slim',['slim']);
+//   gulp.watch('render/html/index.html',['premailer']);
+//   // BV
+//   gulp.watch('src/BV/scss/*.scss',['sass2']);
+//   gulp.watch('render/html/BV/index.html',['premailerBV']);
+// })
