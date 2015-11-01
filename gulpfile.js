4a8b9613700c57c9a8626a68563a1d6e4322bfa2
Manu Lannoy <devries.webdesigner@gmail.com>
2015-11-01 12:45:04 +0100

    ajout npm:rename+path+using+clean; todo > change scss.dest in css.dist

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,13 @@ var gulp    = require('gulp'),
 browserSync = require('browser-sync'),
 slim        = require("gulp-slim"),
 sass        = require('gulp-sass'),
-premailer   = require('gulp-premailer');
+premailer   = require('gulp-premailer'),
+path        = require('path'),
+fs          = require('fs'),
+rename      = require('gulp-rename'),
+using       = require('gulp-using'),
+clean       = require('gulp-clean');
+// imgmin    = require('gulp-imagemin'),
 
 // var dist = ( 'dist/' );
 /*=================================
@@ -25,18 +31,19 @@ gulp.task('browserSync',function () {
 
 // cp img task
 gulp.task('img', function() {
-  return gulp.src(['src/**/images/*.jpg', 'src/**/images/*.gif', 'src/**/images/*.png'])
+  return gulp.src(['src/**/images/*.{png,jpg,gif}'])
   // .pipe(npm())
   .pipe(gulp.dest('render'))
 })
 // sass task
 gulp.task('sass', function() {
-  return gulp.src('src/**/scss/*.scss')
+  return gulp.src('src/**/scss/*.scss')//<, {base: './css/'}
   .pipe(sass())
+  // .pipe(rename("./css/css.css"))
   .pipe(gulp.dest('render'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+  // .pipe(browserSync.reload({
+  //   stream: true
+  // }))
 })
 // slim task
 gulp.task('slim', function () {
@@ -44,23 +51,25 @@ gulp.task('slim', function () {
   .pipe(slim({
     pretty: true
   }))
+  .pipe(using())
   .pipe(gulp.dest('render'))
   // .pipe(browserSync.reload({
   //   stream: true
   // }));
 });
 // premailer task
-// gulp.task('premailer', function () {
-//   gulp.src('src/**/html/index.html')
-//   .pipe(premailer())
-//   .pipe(gulp.dest('src/'));
-// });
+gulp.task('premailer', function () {
+  gulp.src('render/**/slim/*.html')
+  .pipe(premailer())
+  .pipe(gulp.dest('render'));
+});
 
 // lancement > fonction watch
-gulp.task( 'build',['img','slim','sass'], function() {
+gulp.task( 'build',['img','slim','sass','premailer'], function() {
   // gulp.watch( 'browserSync','slim', 'sass', 'imgs' );
   gulp.watch('src/**/slim/*.slim',['slim','img']);
   gulp.watch('src/**/scss/*.scss',['sass']);
+  // gulp.watch('render/**/slim/*.html',['premailer']);
   // gulp.watch('src/**/images/*.scss',['img']);
 });
 
