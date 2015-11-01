96a5699e346b42a2145d7cd8c434e0b316e42a86
Manu Lannoy <devries.webdesigner@gmail.com>
2015-11-01 15:42:35 +0100

    test gulp.dest

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
 /*----------  dependance  > package.json > node_modules  ----------*/
 var gulp    = require('gulp'),
 browserSync = require('browser-sync'),
+dest        = require('gulp-dest'),
 slim        = require("gulp-slim"),
 sass        = require('gulp-sass'),
 premailer   = require('gulp-premailer'),
@@ -16,38 +17,52 @@ clean       = require('gulp-clean');
 // imgmin    = require('gulp-imagemin'),
 
 // var dist = ( 'dist/' );
+// source and distribution folders
+var  src = 'src/';
+var dist = path.resolve( 'render/' );
 /*=================================
 =            task init            =
 =================================*/
 
 // browser-sync task
-gulp.task('browserSync',function () {
-  browserSync({
-    server: {
-      baseDir: 'render/BV/html/'
-    }
-  })
-})
+// gulp.task('browserSync',function () {
+//   browserSync({
+//     server: {
+//       baseDir: 'render/BV/html/'
+//     }
+//   })
+// })
 
-// cp img task
+// cp img folder
 gulp.task('img', function() {
-  return gulp.src(['src/**/images/*.{png,jpg,gif}'])
+  return gulp.src([src+'**/images/*.{png,jpg,gif}'])
+  // .pipe(npm())
+  .pipe(gulp.dest('render'))
+})
+// cp css folder
+gulp.task('cssfold', function() {
+  return gulp.src(src+'**/css/*.css')
   // .pipe(npm())
   .pipe(gulp.dest('render'))
 })
 // sass task
 gulp.task('sass', function() {
-  return gulp.src('src/**/scss/*.scss')//<, {base: './css/'}
+  return gulp.src(src+'**/scss/*.scss')//<, {base: './css/'}
   .pipe(sass())
-  // .pipe(rename("./css/css.css"))
-  .pipe(gulp.dest('render'))
+  // .pipe(rename("css/styles.css"))
+  // .pipe(dest(':scss/:name.css', {scss: 'css'})) // dest>render=root
+  // .pipe(gulp.dest('render'))
+  // .pipe(gulp.dest(src))
+  // .pipe(gulp.dest(dist+'css'))
+  .pipe(gulp.dest('css/'))
+  .pipe(using())
   // .pipe(browserSync.reload({
   //   stream: true
   // }))
 })
 // slim task
 gulp.task('slim', function () {
-  return gulp.src('src/**/slim/*.slim')
+  return gulp.src(src+'**/slim/*.slim')
   .pipe(slim({
     pretty: true
   }))
@@ -67,8 +82,8 @@ gulp.task('premailer', function () {
 // lancement > fonction watch
 gulp.task( 'build',['img','slim','sass','premailer'], function() {
   // gulp.watch( 'browserSync','slim', 'sass', 'imgs' );
-  gulp.watch('src/**/slim/*.slim',['slim','img']);
-  gulp.watch('src/**/scss/*.scss',['sass']);
+  gulp.watch(src+'**/slim/*.slim',['slim','img']);
+  gulp.watch(src+'**/scss/*.scss',['sass']);
   // gulp.watch('render/**/slim/*.html',['premailer']);
   // gulp.watch('src/**/images/*.scss',['img']);
 });
